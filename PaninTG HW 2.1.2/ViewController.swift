e68f6e996bfaaa475f7d002fc453b90cb0dc32b7
TarasMaraz <taras.panin@gmail.com>
2021-05-19 20:39:34 +0300

    app done

--- PaninTG HW 2.1.2/ViewController.swift
+++ PaninTG HW 2.1.2/ViewController.swift
@@ -11,14 +11,56 @@ class ViewController: UIViewController {
 
     var trafficLight: Int?
     
+    @IBOutlet var redLight: UIStackView!
     
+    @IBOutlet var yellowLight: UIStackView!
+    
+    @IBOutlet var greenLight: UIStackView!
+    
+    @IBOutlet var buttonStart: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        redLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
+        
+        redLight.layer.cornerRadius = redLight.frame.size.width / 2
+        redLight.clipsToBounds = true
+        yellowLight.layer.cornerRadius = yellowLight.frame.size.width / 2
+        yellowLight.clipsToBounds = true
+        greenLight.layer.cornerRadius = greenLight.frame.size.width / 2
+        greenLight.clipsToBounds = true
+        
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func buttPres(_ sender: Any) {
+        buttonStart.setTitle("NEXT", for: .normal)
+        if  let trafficLights = trafficLight {
+            redLight.alpha = 0.3
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 0.3
+            
+            switch trafficLights {
+            case 1:
+                trafficLight = 2
+                yellowLight.alpha = 1
+            case 2:
+                trafficLight = 3
+                greenLight.alpha = 1
+            default:
+                trafficLight = 1
+                redLight.alpha = 1
+                break
+            }
+                } else {
+                        trafficLight = 1
+                            redLight.alpha = 1
+            }
+        }
+    }
+    
 
-}
 
