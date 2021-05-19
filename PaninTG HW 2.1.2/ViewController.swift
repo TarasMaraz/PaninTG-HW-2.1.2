f80e066413d76c199e21790e50e9cef4474cb8d0
TarasMaraz <taras.panin@gmail.com>
2021-05-19 20:20:23 +0300

    local

--- PaninTG HW 2.1.2/ViewController.swift
+++ PaninTG HW 2.1.2/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var trafficLight: Int?
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
